49d0e767e5371896da3f887902f71d6982d0d445
RohitBhange <rohitbhnage@gmail.com>
2025-06-09 23:11:51 +0530

    Create multiplytwono.java

---
+++ multiplytwono.java
@@ -0,0 +1,10 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        int result = a * b;
+
+        System.out.println("Multiplication: " + result);
+    }
+}
+
