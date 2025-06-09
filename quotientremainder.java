bb98286ce5486df5cd785530234fcbc60f73d9cb
RohitBhange <rohitbhnage@gmail.com>
2025-06-09 23:24:32 +0530

    Create quotientremainder.java

---
+++ quotientremainder.java
@@ -0,0 +1,11 @@
+public class Divide {
+    public static void main(String[] args) {
+        int dividend = 20, divisor = 3;
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+}
